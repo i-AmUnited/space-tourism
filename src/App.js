44cb579f3b08dcb36f22d69c46ba2059d95e1ee6
tobi <tobiamusa94@gmail.com>
2023-01-21 17:21:18 +0100

    upload v2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/HomePage/Home';
+import Destination from "./Components/Destination/Destination"
+import Crew from './Components/Crew/Crew';
+import Tech from './Components/Technology/Technology';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Destination" element={<Destination />}></Route>
+        <Route path="/Crew" element={<Crew />}></Route>
+        <Route path="/Technology" element={<Tech />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
